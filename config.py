dc77cc2eca16896ba08109a3d2dc2886bb5253a9
darkkingse <64732104+darkkingse@users.noreply.github.com>
2022-07-08 20:35:48 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,12 @@
+access_token = "vk1.a.aRT47WvAsNhhLo1qDBs1DhdCh9Jmucqt3eVxXwTM6LZtghlORGGYhFeV72U6cvMUsSpSJOH-JQVE5l1rpHwftB0zx8nQcTI-IZa7oF2fzrfh9sOFutUgOupPalSk5LpWjq8XT9jtlXF0fpexgwQNpZ2zNcTXUNu1DShIt9eVjYkPC9-VzrpTGMouXFymeIb0"  # токен пользователя (vk.cc/a6dCgm)
+log_messages = True  # логировать ли сообщения
+
+prefixes = {  # префиксы, перед сообщениями
+    "error": "❗",  # Ошибка при использовании
+    "invalid": "⚠️",  # Какой-то недочет в запросе
+    "process": "⚡ ",  # Испоьзуется когда процесс может выполняться долгое время
+    "success": "✅ ",  # Используется когда все прошло успешно
+    "success_no": "❎"  # Используется когда все прошло успешно, но ответ отрицательный
+}
+
+odeanon_token = False
